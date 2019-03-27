ef766b7f7bb760bfa58b19403c2252658aa2d133
Vu Hoang Quan <vhquan@apcs.vn>
2019-03-27 21:52:55 +0700

    fix code for samples with 3 skeleton per frame

--- regenerate_csv_input_file.py
+++ regenerate_csv_input_file.py
@@ -1,3 +1,5 @@
+import os
+
 def createDescCsvFile():
 	datasetFolderPath = 'E:\\Thesis\\Skeleton dataset\\NTU RGB+D Dataset\\nturgb+d_skeletons'
 	file = open("desc.csv", "w")
@@ -34,9 +36,44 @@ def createDescCsvFile():
 		file.write(rowData[i] + '\n')
 	file.close()
 			
-def createSklCsvFile():
+def createSklCsvFile_3_25_2():
 	datasetFolderPath = 'E:\\Thesis\\Skeleton dataset\\NTU RGB+D Dataset\\nturgb+d_skeletons'
-	file = open("skl.csv", "w")
+	file = open("skl_2.csv", "w")
+	brokenFileList = open("samples_with_missing_skeletons_fixed.txt", "r").read()
+	rowData = ""
+	lineNumer = 0
+	for f in os.listdir(datasetFolderPath):
+		if f not in brokenFileList:
+			print("processing file : " + f)
+			datasetFilePath = os.path.join(datasetFolderPath, f)
+			dataFile = open(datasetFilePath, "r").readlines()
+			frameCount = int(dataFile[lineNumer])
+			lineNumer = lineNumer + 1
+			for i in range(frameCount):
+				bodyCount = int(dataFile[lineNumer])
+				for j in range(bodyCount):
+					lineNumer = lineNumer + 2
+					if j == 2:
+						lineNumer = lineNumer + 25
+						break
+					jointCount = int(dataFile[lineNumer])
+					for k in range(jointCount):
+						lineNumer = lineNumer + 1
+						jointInfo = dataFile[lineNumer].split()
+						rowData = rowData + jointInfo[0] + ',' + jointInfo[1] + ',' + jointInfo[2] + ','
+				if bodyCount == 1:
+					for h in range(75):
+						rowData = rowData + '0' + ','
+				lineNumer = lineNumer + 1
+				rowData = rowData[:-1]
+				file.write(rowData + '\n')
+				rowData = ""
+			lineNumer = 0
+			rowData = ""
+
+def createSklCsvFile_3_25_3():
+	datasetFolderPath = 'E:\\Thesis\\Skeleton dataset\\NTU RGB+D Dataset\\nturgb+d_skeletons'
+	file = open("skl_3.csv", "w")
 	brokenFileList = open("samples_with_missing_skeletons_fixed.txt", "r").read()
 	rowData = ""
 	lineNumer = 0
@@ -56,8 +93,9 @@ def createSklCsvFile():
 						lineNumer = lineNumer + 1
 						jointInfo = dataFile[lineNumer].split()
 						rowData = rowData + jointInfo[0] + ',' + jointInfo[1] + ',' + jointInfo[2] + ','
-				if bodyCount == 1:
-					for h in range(75):
+				if bodyCount < 3:
+					zeroLineNumber = (3 - bodyCount)*75
+					for h in range(zeroLineNumber):
 						rowData = rowData + '0' + ','
 				lineNumer = lineNumer + 1
 				rowData = rowData[:-1]
@@ -66,5 +104,22 @@ def createSklCsvFile():
 			lineNumer = 0
 			rowData = ""
 			
-createDescCsvFile()
-createSklCsvFile()
+def samplesWith3SkeletonPerFrame():
+	datasetFolderPath = 'E:\\Thesis\\Skeleton dataset\\NTU RGB+D Dataset\\nturgb+d_skeletons'
+	file = open("samples_with_3_skeleton_per_frame.txt", "w")
+	brokenFileList = open("samples_with_missing_skeletons_fixed.txt", "r").read()
+	lineNumer = 1
+	for f in os.listdir(datasetFolderPath):
+		if f not in brokenFileList:
+			datasetFilePath = os.path.join(datasetFolderPath, f)
+			dataFile = open(datasetFilePath, "r").readlines()
+			bodyCount = int(dataFile[lineNumer])
+			if bodyCount > 2:
+				print(f)
+				file.write(f + '\n')
+			
+#createDescCsvFile()
+#createSklCsvFile()
+#samplesWith3SkeletonPerFrame()
+#createSklCsvFile_3_25_3()
+createSklCsvFile_3_25_2()
